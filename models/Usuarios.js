77d202dedd00ccc5d0129f44b18b757da9aabc8e
Daniel Montiel <danielponce1397@gmail.com>
2024-07-16 21:56:24 -0600

    Correccion id usuario

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -2,6 +2,12 @@ import db from "../config/db.js";
 import { DataTypes } from "sequelize";
 
 const Usuarios = db.define('usuarios',{
+    id: {
+        type:DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
     nombre: {
         type: DataTypes.STRING,
         allowNull: false
